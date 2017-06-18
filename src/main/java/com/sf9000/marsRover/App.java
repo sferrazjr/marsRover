5f1554e6c44d4ba2370ded96da3a1047405d781c
Sergio Ferraz <sferrazjr@gmail.com>
2017-06-18 13:30:18 +0200

    first commit

---
+++ src/main/java/com/sf9000/marsRover/App.java
@@ -0,0 +1,13 @@
+package com.sf9000.marsRover;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
